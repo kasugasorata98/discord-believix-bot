81c6ccbeff0c2787850a2ab5ffdab472ede19340
kasugasorata98 <jeromykho98@gmail.com>
2022-06-23 23:26:51 +0800

    c

--- src/modules/translation/translation.controller.ts
+++ src/modules/translation/translation.controller.ts
@@ -11,7 +11,7 @@ class TranslationController {
     const detectedLanguage = await this.translationService.detectLanguage(
       message
     );
-    if (detectedLanguage !== "es" && detectedLanguage !== 'pt') return;
+    if (detectedLanguage === 'en') return;
     console.log("Detected Language: ", detectedLanguage);
     console.log("Translating to English...");
     const translation = await this.translationService.translateToEnglish(
