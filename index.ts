20021cdddd6accb29f95975fe4818cf9fa15ed6b
kasugasorata98 <jeromykho98@gmail.com>
2022-07-27 06:15:40 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,20 +1,19 @@
 import { config } from "./src/config";
-console.log(config.environment)
-// import MongooseClient from "./src/lib/MongooseClient";
-// import BotController from "./src/modules/bot/bot.controller";
+import MongooseClient from "./src/lib/MongooseClient";
+import BotController from "./src/modules/bot/bot.controller";
 
-// MongooseClient
-//   .connect(config.mongoDBString)
-//   .then(async (res) => {
-//     console.log("MongoDB connected to " + res.connections[0].name);
+MongooseClient
+  .connect(config.mongoDBString)
+  .then(async (res) => {
+    console.log("MongoDB connected to " + res.connections[0].name);
 
-//     const botController = new BotController();
-//     botController.init();
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+    const botController = new BotController();
+    botController.init();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-// process.on("uncaughtException", async (error) => {
-//   console.log(error);
-// });
+process.on("uncaughtException", async (error) => {
+  console.log(error);
+});
